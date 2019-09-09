2cb27e4a0f7fa226a5d40b0534dcb02f15208e43
Karl Syvert Løland <karl.syvert@myerart.net>
2019-09-09 07:12:50 +0200

    Minor cleanup

--- TrollCode.Nordnet.API/NordnetApi.cs
+++ TrollCode.Nordnet.API/NordnetApi.cs
@@ -27,8 +27,10 @@ namespace TrollCode.Nordnet.API
         public NordnetApi(RSACryptoServiceProvider key, string baseAddress)
         {
             publicKey = key;
-            client = new HttpClient();
-            client.BaseAddress = new Uri(baseAddress);
+            client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
             client.DefaultRequestHeaders.Add("Accept", "application/json");
             ServiceName = "NEXTAPI";
         }
@@ -93,8 +95,6 @@ namespace TrollCode.Nordnet.API
 
         }
 
-        //GET 
-
         public async Task QueryIntruments()
         {
             HttpResponseMessage response = await client.GetAsync($"/next/2/instruments?query=");
@@ -150,85 +150,86 @@ namespace TrollCode.Nordnet.API
 
         public void ConnectToFeed(FeedInformation information, string sessionid)
         {
-            TcpClient client = new TcpClient(information.Hostname, information.Port);
-            Console.WriteLine("Client connected.");
-            
-
-            SslStream sslStream = new SslStream(client.GetStream(),false, new RemoteCertificateValidationCallback((object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+            using (TcpClient client = new TcpClient(information.Hostname, information.Port))
             {
-                //Dont validate for now. Add later!
-                return true;
+                Console.WriteLine("Client connected.");
 
-            }), null);
+                //NOTE to self: Does leaveInnerStreamOpen means that the inner TcpClient gets closed when the SslStream closes?
+                using (SslStream sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback((object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) =>
+                {
+                    //Dont validate for now. Add later!
+                    return true;
+
+                }), null))
+                {
+                    try
+                    {
+                        sslStream.AuthenticateAsClient("I Will Fail Now");
+                    }
+                    catch (AuthenticationException e)
+                    {
+                        Console.WriteLine("Exception: {0}", e.Message);
+                        if (e.InnerException != null)
+                        {
+                            Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                        }
+                        Console.WriteLine("Authentication failed - closing the connection.");
+                        client.Close();
+                        return;
+                    }
+
+                    sslStream.Write(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(new Command
+                    {
+                        cmd = "login",
+                        args = new Dictionary<string, string>
+                        {
+                            {"session_key", sessionid}
+                        }
+                    }) + "\n"));
+                    sslStream.Flush();
+
+                    sslStream.Write(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(new Command
+                    {
+                        //{"market_id":14,"country":"DK","name":"Nasdaq OMX Copenhagen"},
+                        cmd = "subscribe", //"args":{"t":"price", "i":"1869", "m":30}}
+                        args = new Dictionary<string, string>
+                        {
+                            {"t", "price"},
+                            {"i", "1869" },
+                            {"m", "14"}
+                        }
+                    }) + "\n"));
+                    sslStream.Flush();
+
+                    byte[] buffer = new byte[2048];
+                    StringBuilder messageData = new StringBuilder();
+                    int bytes = -1;
+                    do
+                    {
+                        bytes = sslStream.Read(buffer, 0, buffer.Length);
+
+                        Decoder decoder = Encoding.ASCII.GetDecoder();
+                        char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                        decoder.GetChars(buffer, 0, bytes, chars, 0);
+
+                        Console.WriteLine($"{DateTime.UtcNow.ToString("mm:ss")}: Got {bytes} bytes - {new string(chars)}");
+
+                        try
+                        {
+                            FeedResponse response = JsonConvert.DeserializeObject<FeedResponse>(new string(chars));
 
 
-            try
-            {
-                sslStream.AuthenticateAsClient("I Will Fail Now");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Could not parse feed response");
+                            Console.WriteLine(ex.Message);
+                        }
+                    } while (bytes != 0);
+
+                    Console.WriteLine($"The server has disconnected - Stopping");
+                }
             }
-            catch (AuthenticationException e)
-            {
-                Console.WriteLine("Exception: {0}", e.Message);
-                if (e.InnerException != null)
-                {
-                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
-                }
-                Console.WriteLine("Authentication failed - closing the connection.");
-                client.Close();
-                return;
-            }
-
-            sslStream.Write(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(new Command
-            {
-                cmd = "login",
-                args = new Dictionary<string, string>
-                {
-                    {"session_key", sessionid}
-                }
-            }) + "\n"));
-            sslStream.Flush();
-
-            sslStream.Write(Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(new Command
-            {
-                //{"market_id":14,"country":"DK","name":"Nasdaq OMX Copenhagen"},
-                cmd = "subscribe", //"args":{"t":"price", "i":"1869", "m":30}}
-                args = new Dictionary<string, string>
-                {
-                    {"t", "price"},
-                    {"i", "1869" },
-                    {"m", "14"}
-                }
-            }) + "\n"));
-            sslStream.Flush();
-
-            byte[] buffer = new byte[2048];
-            StringBuilder messageData = new StringBuilder();
-            int bytes = -1;
-            do
-            {
-                bytes = sslStream.Read(buffer, 0, buffer.Length);
-
-                Decoder decoder = Encoding.ASCII.GetDecoder();
-                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
-                decoder.GetChars(buffer, 0, bytes, chars, 0);
-
-                Console.WriteLine($"{DateTime.UtcNow.ToString("mm:ss")}: Got {bytes} bytes - {new string(chars)}");
-
-                try
-                {
-                    FeedResponse response = JsonConvert.DeserializeObject<FeedResponse>(new string(chars));
-
-
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine("Could not parse feed response");
-                    Console.WriteLine(ex.Message);
-                }
-            } while (bytes != 0);
-
-            Console.WriteLine($"The server has disconnected - Stopping");
-
         }
     }
 }
